9762ec2f472738610f7fa77d789ac7aef9f86e11
Pavel B <paul702600@gmail.com>
2025-11-17 15:11:26 +0300

    create VITE_BASE_URL in env

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig, loadEnv } from 'vite'
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   process.env = { ...process.env, ...env }
-  const proxyUrl = process.env.BASE_URL || 'https://b8bc855c293e.ngrok-free.app'
+  const proxyUrl = process.env.VITE_BASE_URL
 
   console.log('👉 Loaded BASE_URL:', proxyUrl)
 
